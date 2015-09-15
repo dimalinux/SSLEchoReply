85b32cedd529d1562ad01ce255f00b1e0623832c
Дмитрий Холодов <github@sec.to>
2015-09-15 14:33:26 -0500

    added ability for client to skip the use of SSL

--- EchoClient/src/main/java/to/noc/sslechoreply/client/EchoClient.java
+++ EchoClient/src/main/java/to/noc/sslechoreply/client/EchoClient.java
@@ -1,9 +1,11 @@
 package to.noc.sslechoreply.client;
 
+import javax.net.SocketFactory;
 import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
+import java.net.Socket;
 
 public class EchoClient {
 
@@ -14,19 +16,26 @@ public class EchoClient {
         }
         try {
             String hostname = args[0];
+
+            //
+            //  Hack alert. Using a negative port number uses plain TCP instead of SSL.
+            //
             int port = Integer.parseInt(args[1]);
+            boolean skipSSL = port < 0;
+            port = Math.abs(port);
 
-            System.out.println("About to connect to '" + hostname + "' on port " + port);
+            System.out.println("About to connect to '" + hostname + "' on port " + port + " using SSL=" + !skipSSL);
 
-            SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            SSLSocket socket = (SSLSocket) socketFactory.createSocket(hostname, port);
+            SocketFactory socketFactory = skipSSL ? SocketFactory.getDefault() : SSLSocketFactory.getDefault();
+            Socket socket = socketFactory.createSocket(hostname, port);
 
-            // Hostname verification is not done by default in Java with raw SSL connections.
-            // The next 3 lines enable it.
-            SSLParameters sslParams = new SSLParameters();
-            sslParams.setEndpointIdentificationAlgorithm("HTTPS");
-            socket.setSSLParameters(sslParams);
-
+            if (!skipSSL) {
+                // Hostname verification is not done by default in Java with raw SSL connections.
+                // The next 3 lines enable it.
+                SSLParameters sslParams = new SSLParameters();
+                sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+                ((SSLSocket)socket).setSSLParameters(sslParams);
+            }
             socket.setTcpNoDelay(true);
 
             final BufferedReader sockReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -40,6 +49,8 @@ public class EchoClient {
                     String inputLine;
                     try {
                         while ((inputLine = inputReader.readLine()) != null) {
+                            if (inputLine.trim().equals("exit"))
+                                break;
                             sockWriter.println(inputLine);
                             sockWriter.flush();
                         }
