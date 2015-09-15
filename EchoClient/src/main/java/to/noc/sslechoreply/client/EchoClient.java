983360f3b12b47b611701372d88851ed88ac367b
Дмитрий Холодов <github@sec.to>
2015-09-15 14:59:27 -0500

    improved exit handling

--- EchoClient/src/main/java/to/noc/sslechoreply/client/EchoClient.java
+++ EchoClient/src/main/java/to/noc/sslechoreply/client/EchoClient.java
@@ -6,6 +6,7 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class EchoClient {
 
@@ -17,27 +18,14 @@ public class EchoClient {
         try {
             String hostname = args[0];
 
-            //
             //  Hack alert. Using a negative port number uses plain TCP instead of SSL.
-            //
             int port = Integer.parseInt(args[1]);
             boolean skipSSL = port < 0;
             port = Math.abs(port);
 
             System.out.println("About to connect to '" + hostname + "' on port " + port + " using SSL=" + !skipSSL);
 
-            SocketFactory socketFactory = skipSSL ? SocketFactory.getDefault() : SSLSocketFactory.getDefault();
-            Socket socket = socketFactory.createSocket(hostname, port);
-
-            if (!skipSSL) {
-                // Hostname verification is not done by default in Java with raw SSL connections.
-                // The next 3 lines enable it.
-                SSLParameters sslParams = new SSLParameters();
-                sslParams.setEndpointIdentificationAlgorithm("HTTPS");
-                ((SSLSocket)socket).setSSLParameters(sslParams);
-            }
-            socket.setTcpNoDelay(true);
-
+            final Socket socket = createSocketToEchoServer(hostname, port, skipSSL);
             final BufferedReader sockReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             final PrintWriter sockWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
@@ -49,11 +37,13 @@ public class EchoClient {
                     String inputLine;
                     try {
                         while ((inputLine = inputReader.readLine()) != null) {
-                            if (inputLine.trim().equals("exit"))
+                            if (inputLine.trim().equals("exit")) {
                                 break;
+                            }
                             sockWriter.println(inputLine);
                             sockWriter.flush();
                         }
+                        socket.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                         System.exit(1);
@@ -63,13 +53,34 @@ public class EchoClient {
             ).start();
 
 
-            String sockLine;
-            while ( (sockLine = sockReader.readLine()) != null ) {
-                System.out.println("Echo reply: " + sockLine);
+            try {
+                String sockLine;
+                while ((sockLine = sockReader.readLine()) != null) {
+                    System.out.println("Echo reply: " + sockLine);
+                }
+            } catch (SocketException ex) {
+                // exception normal, since the standard input reader closes the socket
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Socket createSocketToEchoServer(String hostname, int port, boolean skipSSL) throws IOException {
+        SocketFactory socketFactory = skipSSL ? SocketFactory.getDefault() : SSLSocketFactory.getDefault();
+        final Socket socket = socketFactory.createSocket(hostname, port);
+
+        if (!skipSSL) {
+            // Hostname verification is not done by default in Java with raw SSL connections.
+            // The next 3 lines enable it.
+            SSLParameters sslParams = new SSLParameters();
+            sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+            ((SSLSocket)socket).setSSLParameters(sslParams);
+        }
+        // it's interactive chat, so we don't want buffering delays
+        socket.setTcpNoDelay(true);
+
+        return socket;
     }
 }
